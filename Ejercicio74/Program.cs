30854911cf187fe9039a7fb50b8d3a83519e2936
FelixManuelEspinoza <81267662+FelixManuelEspinoza@users.noreply.github.com>
2022-10-17 16:16:53 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio74/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+namespace Ejercicio74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            Stack colaPalabras = new Stack();
+            palabra = Console.ReadLine();
+            while (palabra != "")
+            {
+                colaPalabras.Push(palabra);
+                Console.WriteLine("Ingrese la siguiente palabra");
+                palabra = Console.ReadLine();
+            }
+            while (colaPalabras.Count > 0)
+            {
+                Console.WriteLine((string)colaPalabras.Pop());
+            }
+            Console.ReadKey();
+        }
+    }
+}
